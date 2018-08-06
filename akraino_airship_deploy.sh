03a7f3ab126af987da1110ad8e537786d5e5c949
davidplunkett <dp7642@att.com>
2018-08-05 20:08:06 -0400

    enable dhcp and add nameserver to public
    Change-Id: Ic7a86dfbd00f852e016a1391641ec0cc6d65fb90
    Signed-off-by: davidplunkett <dp7642@att.com>

--- akraino_airship_deploy.sh
+++ akraino_airship_deploy.sh
@@ -48,6 +48,8 @@ if [ $pods -gt 75 ]; then
 
         ~/deploy/airship-in-a-bottle/manifests/dev_single_node/run_openstack_cli.sh quota set --cores $CPU --ram $MEM admin
         ~/deploy/airship-in-a-bottle/manifests/dev_single_node/run_openstack_cli.sh quota show admin
+        ~/deploy/airship-in-a-bottle/manifests/dev_single_node/run_openstack_cli.sh subnet set --dhcp --dns-nameserver 8.8.8.8 public
+        ~/deploy/airship-in-a-bottle/manifests/dev_single_node/run_openstack_cli.sh subnet show public
         set +x
         echo "Environment Deployed: Please login to environment to validate further"
         exit 0
